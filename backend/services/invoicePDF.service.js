8722ea0b2a7ec6c181ad2be5ffb098d6427a47fa
Markus Rossmann <markus.rossmann@edu.fh-joanneum.at>
2025-12-18 22:13:53 +0100

    feat: add service skeleton

---
+++ backend/services/invoicePDF.service.js
@@ -0,0 +1,21 @@
+import express from "express";
+import pool from "../db.js";
+
+const puppeteer = require("puppeteer");
+const hbs = require("handlebars");
+const fs = require("fs-extra");
+const path = require("path");
+
+
+class InvoiceService {
+
+
+}
+
+
+// // to wait for font api to finish loading
+// await page.setContent(finalHtml, {
+//     waitUntil: 'networkidle0'
+// });
+
+module.exports = new InvoiceService();
